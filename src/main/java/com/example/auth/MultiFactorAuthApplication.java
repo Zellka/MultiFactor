269029ac59586a1747951fb17509449d633d8e08
Zellka <ilona.zelka@gmail.com>
2022-03-18 16:58:08 +0300

    Initial commit

---
+++ src/main/java/com/example/auth/MultiFactorAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiFactorAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiFactorAuthApplication.class, args);
+	}
+
+}
